35d5dd2ef7fd69959659468895d368d83bd8aa27
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-11-02 16:47:23 +0100

    added title

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import DataTable from "./data-table";
 import { columns } from "./columns";
 import { api } from "~/trpc/server";
+import { Button } from "~/app/_components/ui/button";
+import { Plus } from "lucide-react";
 
 const Page = async () => {
     const data = await api.stripe.getProducts.query();
 
     return (
         <div className="">
-            <DataTable columns={columns} data={data} />
+            <div className="flex justify-between px-4 py-2">
+                <span className="text-3xl font-bold">Products table</span>
+
+                <Button className="flex gap-2" size="sm">
+                    <Plus />
+                    Create product
+                </Button>
+            </div>
+            <div className="">
+                <DataTable columns={columns} data={data} />
+            </div>
         </div>
     );
 };
