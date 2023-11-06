e19020190bf7e5fcdce25081a99ea7a3e434d874
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-11-06 09:40:45 +0100

    added new variant

--- src/app/_components/ui/button.tsx
+++ src/app/_components/ui/button.tsx
@@ -17,6 +17,8 @@ const buttonVariants = cva(
                 secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
                 ghost: "hover:bg-accent hover:text-accent-foreground",
                 link: "text-primary underline-offset-4 hover:underline",
+                outlinePrimary:
+                    "border-2 border-foreground uppercase hover:text-primary hover:border-primary",
             },
             size: {
                 default: "h-10 px-4 py-2",
