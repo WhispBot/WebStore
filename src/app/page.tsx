da1f107b232ce90aa50c30892a003081c47bff9b
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-11-06 09:39:30 +0100

    added layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@
 // import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 import ProductCard from "./_components/product-card";
+import { Button, buttonVariants } from "./_components/ui/button";
+import Link from "next/link";
+import { cn } from "~/lib/utils";
 
 export default async function Home() {
     const res = await api.stripe.products.query();
@@ -12,12 +15,89 @@ export default async function Home() {
     // const session = await getServerAuthSession();
 
     return (
-        <main className="">
-            <div className="flex gap-2">
-                {res.map((product) => (
-                    <ProductCard product={product} key={product.id} />
-                ))}
+        <main className="container flex-grow">
+            <div className="grid h-[700px] max-h-full grid-cols-4 grid-rows-3 gap-2">
+                <div className="col-span-3 row-span-2 flex flex-col items-center justify-center gap-8 border bg-muted/25">
+                    <span className="text-4xl font-semibold uppercase">
+                        {" - featured -"}
+                    </span>
+                    <Link
+                        href={"/"}
+                        className={cn(buttonVariants({ variant: "outlinePrimary" }), "")}
+                    >
+                        Shop now
+                    </Link>
+                </div>
+                <div className="row-span-2  border bg-muted/25 p-10">
+                    <div className="flex h-full w-full flex-col items-center justify-center gap-8 border-2 ">
+                        <span className="text-4xl font-semibold uppercase">
+                            {" - 50% off-"}
+                        </span>
+                        <Link
+                            href={"/"}
+                            className={cn(
+                                buttonVariants({ variant: "outlinePrimary" }),
+                                ""
+                            )}
+                        >
+                            Shop now
+                        </Link>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-8 border bg-muted/25">
+                    <span className="text-2xl font-semibold uppercase">
+                        {" - Stickers -"}
+                    </span>
+                    <Link
+                        href={"/"}
+                        className={cn(buttonVariants({ variant: "outlinePrimary" }), "")}
+                    >
+                        Shop now
+                    </Link>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-8 border bg-muted/25">
+                    <span className="text-2xl font-semibold uppercase">
+                        {" - Posters -"}
+                    </span>
+                    <Link
+                        href={"/"}
+                        className={cn(buttonVariants({ variant: "outlinePrimary" }), "")}
+                    >
+                        Shop now
+                    </Link>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-8 border bg-muted/25">
+                    <span className="text-2xl font-semibold uppercase">
+                        {" - Clothing -"}
+                    </span>
+                    <Link
+                        href={"/"}
+                        className={cn(buttonVariants({ variant: "outlinePrimary" }), "")}
+                    >
+                        Shop now
+                    </Link>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-8 border bg-muted/25">
+                    <span className="text-2xl font-semibold uppercase">
+                        {" - Accessories -"}
+                    </span>
+                    <Link
+                        href={"/"}
+                        className={cn(buttonVariants({ variant: "outlinePrimary" }), "")}
+                    >
+                        Shop now
+                    </Link>
+                </div>
             </div>
+            <div className="py-16 text-center font-bold uppercase">products</div>
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex gap-8">
+                    {res.map((product) => (
+                        <ProductCard product={product} key={product.id} />
+                    ))}
+                </div>
+            </div>
+            <div className="py-16 text-center font-bold uppercase"></div>
         </main>
     );
 }
