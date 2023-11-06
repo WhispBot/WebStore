26b5046e4c335e99c5e193e2b14cb3511ef1daf7
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-11-06 09:39:40 +0100

    change links

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -9,7 +9,7 @@ export default async function Header() {
     const session = await getServerAuthSession();
 
     return (
-        <nav className="flex h-16 select-none items-center justify-between border-b-2 py-4">
+        <nav className="sticky top-0 flex h-16 select-none items-center justify-between border-b-2 bg-background py-4">
             <div className="container flex items-center justify-between">
                 <div className="">
                     <Link href="/" className="font-semibold uppercase">
@@ -17,12 +17,14 @@ export default async function Header() {
                     </Link>
                 </div>
                 <div className="flex gap-8">
-                    <NavLink href="/">Home</NavLink>
-                    <NavLink href="/product">Products</NavLink>
-                    <NavLink href="/about">About</NavLink>
+                    <NavLink href="/product">Stickers</NavLink>
+                    <NavLink href="/product">Posters</NavLink>
+                    <NavLink href="/product">Accessories</NavLink>
+                    <NavLink href="/product">t-shirts</NavLink>
+                    {/* <NavLink href="/about">About</NavLink> */}
                 </div>
                 <div className="flex gap-4">
-                    {/* <ShopingCart /> */}
+                    <ShopingCart />
                     <ProfileDropdown session={session} />
                     <ThemeToggle />
                 </div>
